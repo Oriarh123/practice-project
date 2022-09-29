1587cacb0ac6d5aea5afd8df4dc33aa56309d7aa
oriarh <111208485+oriarh@users.noreply.github.com>
2022-09-28 18:00:04 -0700

    changed the content a little'

--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
 var qualityarray = ["Her Heart","Loyalty","Smart","Loves me","Great Boobs",">Her Beautiful smile","Personality","Great Ass"];
 
 button2.onclick = function(){
-    console.log("Her Boobs")
-    document.getElementById("randomquality").innerText="Her boobs!!!";
+    console.log("Her Smile")
+    document.getElementById("randomquality").innerText="Her Smile!!!";
     document.getElementById("randomquality").style.fontSize ="100px";
-    document.getElementById("smalltext").innerHTML="JK i love everything about her. She is my jaan ka tukra and she is precious";
+    document.getElementById("smalltext").innerHTML="I love everything about her. She is my jaan ka tukra and she is precious";
     document.getElementById("smalltext").style.fontSize = "50px";
-    document.getElementById("smallertext").innerHTML="Janu please talk to me I miss you so much";
     document.getElementById("smallertext").style.fontSize = "25px";
 };
